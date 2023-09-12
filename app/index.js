8e01e19318e06c17e56b142e8b0ebd04d60274b0
fyosetake <fyosetake@gmail.com>
2023-09-12 17:59:21 -0300

    Implementa o uso do 'sequelize' para registrar ponto

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Registro = require('./Registro');
+const sequelize = require('./dbConfig');
 
 const app = express();
 
@@ -7,8 +9,11 @@ app.use(bodyParser.json());
 
 app.post('/registrar-ponto', async (req, res) => {
   try {
-    
-    console.log("Ponto registrado com sucesso!");
+    const { user } = req.body;
+    const timestamp = new Date();
+
+    // Insere um registro na tabela
+    await Registro.create({ user, timestamp });
 
     res.status(200).json({ message: 'Ponto registrado com sucesso.' });
   } catch (error) {
@@ -19,6 +24,8 @@ app.post('/registrar-ponto', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
 });
